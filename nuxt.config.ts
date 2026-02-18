1e63bc7c873c8897c47a5e4672b5e6cdc3927d49
Serafim Vasilev <zzerafim@gmail.com>
2026-02-18 19:24:02 +0700

    chore: create project with stubs

---
+++ nuxt.config.ts
@@ -0,0 +1,7 @@
+export default defineNuxtConfig({
+    ssr: true,
+    srcDir: 'app/',
+    css: ['@/assets/styles/main.scss'],
+    compatibilityDate: '2025-07-15',
+    devtools: { enabled: true },
+});
